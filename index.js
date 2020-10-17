285e0d0bb4f830d03570a91b6b05dd6052ca28df
itacsq <christian.scquizzato@bidocmpany.it>
2020-10-17 11:14:04 +0200

    atom

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express')
+const fs = require('fs')
+const util = require('util')
+const fs_ls = util.promisify(fs.readdir);
 const app = express()
 const port = 8080
 
+/*
 app.get('/', (req, res) => {
   let mex='Hello World 2.0!'
   res.send(mex)
@@ -10,3 +14,10 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+*/
+(async () => {
+  const files = await fs_readdir('.');
+  for (let fn of files) {
+    console.log(fn);
+  }
+})().catch(err => { console.error(err); });
