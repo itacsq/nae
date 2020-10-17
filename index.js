25f81123deb1475f2ecd6364cbfd4f2863a8a411
itacsq <christian.scquizzato@bidocmpany.it>
2020-10-17 07:58:10 +0200

    second commit

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const app = express()
 const port = 8080
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  let mex='Hello World 2.0!'
+  res.send(mex)
 })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
-
